1679d772074d152e84247687cbc316c62568fb8a
Mohammad Arif <arif_iq@yahoo.co.in>
2023-01-22 09:10:24 +0530

    Fill contact, listing detail and my listing pages

--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listing-detail-page',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./listing-detail-page.component.css']
 })
 export class ListingDetailPageComponent {
+  listing: Listing | undefined;
+
+  constructor( private route: ActivatedRoute ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id);
+  }
 
 }
--- src/app/my-listings-page/my-listings-page.component.ts
+++ src/app/my-listings-page/my-listings-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { fakeMyListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-my-listings-page',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-listings-page.component.css']
 })
 export class MyListingsPageComponent {
+  listings: Listing[] = []
+
+  ngOnInit(): void {
+    this.listings = fakeMyListings;
+  }
+
+  onDeleteClicked(listingId: string): void {
+    alert(`Deleting your listing with id ${listingId}`)
+  }
 
 }
