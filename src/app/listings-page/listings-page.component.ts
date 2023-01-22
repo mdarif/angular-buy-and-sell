bf48d102b3cae9f921032d18f891bdcbb409701e
Mohammad Arif <arif_iq@yahoo.co.in>
2023-01-22 08:36:10 +0530

    Add fake data and configure the router module

--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-listings-page',
   templateUrl: './listings-page.component.html',
   styleUrls: ['./listings-page.component.css']
 })
-export class ListingsPageComponent {
+export class ListingsPageComponent implements OnInit {
+  listings: Listing[] = [];
 
+  ngOnInit(): void {
+    this.listings = fakeListings;
+  }
 }
